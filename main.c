8fa12bc32846b7be4a7f7429d368abce3f601f8a
FlorettPaqrett <flore.nardelli@epitech.eu>
2017-04-17 13:10:37 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,3 +1,28 @@
+
+int connct_4()
+{
+  int  winner;
+  
+  winner = loop();
+  if (winner == 1)
+  {
+    printf("Le joueur 1 gagne !\n");
+    return (1);
+  }
+  else if (winner == 2)
+  {
+    printf("Le joueur 2 gagne !\n");
+    return (2);
+  }
+  else if (winner == 0)
+  {
+    printf("Egalité !\n");
+    return (0);
+  }
+  else 
+    retun (84);
+}
+
 int main()
 {
   return (connect_4());
